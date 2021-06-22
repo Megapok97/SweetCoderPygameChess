79293f0b66d6b9d111c707bd1f2cb4440ab68453
Megapok <Megapok97@mail.ru>
2021-06-22 15:30:22 +0800

    Еще больше ООП, очень долго запускается. Нужно оптимизировать

--- chess_items.py
+++ chess_items.py
@@ -8,18 +8,34 @@ class Chessboard:
     def __init__(self, parent_sufface: pg.Surface,
                  cell_qty: int = CELL_QTY, cell_size: int = CELL_SIZE):
         self.__screen = parent_sufface
-        self.__draw_playboard(cell_qty, cell_size)
+        self.__qty = cell_qty
+        self.__size = cell_size
+        self.__all_cells = pg.sprite.Group()
+        self.__prepare_screen()
+        self.__draw_playboard()
         pg.display.update()
 
-    def __draw_playboard(self, cell_qty, cell_size):
-        total_width = cell_qty * cell_size
-        num_filds = self.__create_num_fields(cell_qty, cell_size)
-        fields = self.__create_all_cells(cell_qty, cell_size)
+    def __prepare_screen(self):
+        back_img = pg.image.load(IMG_PATH + WIN_BG_IMG)
+        back_img = pg.transform.scale(back_img, WINDOW_SIZE)
+        self.__screen.blit(back_img, (0, 0))
+
+    def __draw_playboard(self):
+        total_width = self.__qty * self.__size
+        num_filds = self.__create_num_fields()
+        self.__all_cells = self.__create_all_cells()
         num_fields_depth = num_filds[0].get_width()
         playboard_view = pg.Surface((
             2 * num_fields_depth + total_width,
             2 * num_fields_depth + total_width
         ), pg.SRCALPHA)
+
+        back_img = pg.image.load(IMG_PATH + BOARD_BG_IMG)
+        back_img = pg.transform.scale(back_img, (
+            playboard_view.get_width(),
+            playboard_view.get_height()
+        ))
+        playboard_view.blit(back_img, (back_img.get_rect()))
 
         playboard_view.blit(num_filds[0],
                             (0, num_fields_depth))
@@ -29,43 +45,72 @@ class Chessboard:
                             (num_fields_depth, 0))
         playboard_view.blit(num_filds[1],
                             (num_fields_depth, num_fields_depth + total_width))
-        playboard_view.blit(fields,
-                            (num_fields_depth, num_fields_depth))
 
         playboard_rect = playboard_view.get_rect()
         playboard_rect.x += (self.__screen.get_width() - playboard_rect.width) // 2
         playboard_rect.y += (self.__screen.get_height() - playboard_rect.height) // 2
         self.__screen.blit(playboard_view, playboard_rect)
+        cells_offset = (
+            playboard_rect.x + num_fields_depth,
+            playboard_rect.y + num_fields_depth
+        )
+        self.__draw_cells_on_playboard(cells_offset)
 
-
-    def __create_num_fields(self, cell_qty, cell_size):
-        n_lines = pg.Surface((cell_qty * cell_size, cell_size // 3), pg.SRCALPHA)
-        n_rows = pg.Surface((cell_size // 3, cell_qty * cell_size), pg.SRCALPHA)
-        for i in range(0, cell_qty):
+    def __create_num_fields(self):
+        n_lines = pg.Surface((self.__qty * self.__size, self.__size // 3), pg.SRCALPHA)
+        n_rows = pg.Surface((self.__size // 3, self.__qty * self.__size), pg.SRCALPHA)
+        for i in range(0, self.__qty):
             letter = fnt_num.render(LTRS[i], True, WHITE)
-            number = fnt_num.render(str(cell_qty - i), True, WHITE)
+            number = fnt_num.render(str(self.__qty - i), True, WHITE)
             n_lines.blit(letter, (
-                i * cell_size + (cell_size - letter.get_rect().width) // 2,  # X
+                i * self.__size + (self.__size - letter.get_rect().width) // 2,  # X
                 (n_lines.get_height() - letter.get_rect().height) // 2  # Y
                 )
             )
             n_rows.blit(number, (
                 (n_rows.get_width() - letter.get_rect().width) // 2,  # X
-                i * cell_size + (cell_size - number.get_rect().height) // 2  # Y
+                i * self.__size + (self.__size - number.get_rect().height) // 2  # Y
                 )
             )
-        return (n_rows, n_lines)
+        return n_rows, n_lines
 
-
-    def __create_all_cells(self, cell_qty, cell_size):
-        fields = pg.Surface((cell_qty * cell_size, cell_qty * cell_size))
-        is_even_qty = (cell_qty % 2 == 0)
+    def __create_all_cells(self):
+        group = pg.sprite.Group()
+        is_even_qty = (self.__qty % 2 == 0)
         cell_color_index = 1 if is_even_qty else 0
-        for y in range(cell_qty):
-            for x in range(cell_qty):
-                cell = pg.Surface((cell_size, cell_size), pg.SRCALPHA)
-                cell.fill(COLORS[cell_color_index])
-                fields.blit(cell, (x * cell_size, y * cell_size))
+
+        cell_black = pg.image.load(IMG_PATH + COLORS[0])
+        cell_white = pg.image.load(IMG_PATH + COLORS[1])
+        cell_black = pg.transform.scale(cell_black, (self.__size, self.__size))
+        cell_white = pg.transform.scale(cell_white, (self.__size, self.__size))
+        cells = [cell_black, cell_white]
+
+        for y in range(self.__qty):
+            for x in range(self.__qty):
+                cell = Cell(
+                    cell_color_index,
+                    self.__size,
+                    (x, y),
+                    LTRS[x] + str(self.__qty - y)
+                )
+                group.add(cell)
                 cell_color_index ^= True
             cell_color_index = cell_color_index ^ True if is_even_qty else cell_color_index
-        return  fields
+        return group
+
+    def __draw_cells_on_playboard(self, offset):
+        for cell in self.__all_cells:
+            cell.rect.x += offset[0]
+            cell.rect.y += offset[1]
+        self.__all_cells.draw(self.__screen)
+
+
+class Cell(pg.sprite.Sprite):
+    def __init__(self, color_index: int, size: int, coords: tuple, name: str):
+        super().__init__()
+        x, y = coords
+        self.color = color_index
+        self.field_name = name
+        self.image = pg.image.load(IMG_PATH + COLORS[color_index])
+        self.image = pg.transform.scale(self.image, (size, size))
+        self.rect = pg.Rect(x * size, y * size, size, size)
